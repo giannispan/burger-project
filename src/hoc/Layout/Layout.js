0384dcf0a8096131a4493b34b77c0e6b6eca2fc2
giannispan <giannis.panagiotopoulos@gmail.com>
2020-11-05 15:34:59 +0200

    convert layout.js to functional

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Aux from '../Aux/Aux';
 import classes from './Layout.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-	state = {
-		showSideDrawer: false
+const layout = props => {
+	const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+
+	const sideDrawerCloseHandler = () => {
+		setSideDrawerIsVisible(false);
 	}
 
-	sideDrawerCloseHandler = () => {
-		this.setState({showSideDrawer: false});
+	const sideDrawerToggleHandler = () => {
+		setSideDrawerIsVisible(!sideDrawerIsVisible);
 	}
 
-	sideDrawerToggleHandler = () => {
-		this.setState((prevState) => {
-			return {showSideDrawer: !prevState.showSideDrawer}
-		})
-	}
-
-	render() {
-		return (
-			<Aux>
-				<Toolbar
-					isAuth={this.props.isAuthenticated}
-					toggleClicked={this.sideDrawerToggleHandler}/>
-				<SideDrawer
-					isAuth={this.props.isAuthenticated}
-					open={this.state.showSideDrawer}
-					closed={this.sideDrawerCloseHandler} />
-				<main className={classes.Content}>
-					{this.props.children}
-				</main>
-			</Aux>
-		)
-	}
+	return (
+		<Aux>
+			<Toolbar
+				isAuth={props.isAuthenticated}
+				toggleClicked={sideDrawerToggleHandler}/>
+			<SideDrawer
+				isAuth={props.isAuthenticated}
+				open={sideDrawerIsVisible}
+				closed={sideDrawerCloseHandler} />
+			<main className={classes.Content}>
+				{props.children}
+			</main>
+		</Aux>
+	)
 }
 
 const mapStateToProps = state => {
@@ -44,4 +38,4 @@ const mapStateToProps = state => {
 	}
 }
 
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(layout);
